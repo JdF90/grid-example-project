b538116c1c3d629aaea2d4aee69715b57896f1c4
JdF90 <jwmdefockert@gmail.com>
2023-09-04 19:01:21 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <main>
+    <nav class="navigation"></nav>
+    <div class="user-setting"></div>
+    <div class="main-content">
+      <div class="header"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </main>
+`
